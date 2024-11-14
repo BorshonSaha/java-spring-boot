53899da90045d3c8a22c28c133e1fe92779d8637
Borshon Saha <I-588@wgbd.com>
2024-11-14 15:17:04 +0600

    Loosely coupled code using interface

--- 1. Basic-Spring-Framework/learning-spring-framework/src/main/java/com/spring/learning_spring_framework/AppGamingBasicJava.java
+++ 1. Basic-Spring-Framework/learning-spring-framework/src/main/java/com/spring/learning_spring_framework/AppGamingBasicJava.java
@@ -2,15 +2,17 @@ package com.spring.learning_spring_framework;
 
 import com.spring.learning_spring_framework.game.GameRunner;
 import com.spring.learning_spring_framework.game.MarioGame;
+import com.spring.learning_spring_framework.game.PacMan;
 import com.spring.learning_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
-		
-		// var marioGame = new MarioGame();
-		var superContraGame = new SuperContraGame();
-		var gameRunner = new GameRunner(superContraGame);
+
+//		var game = new MarioGame();
+//		var game = new SuperContraGame();
+		var game = new PacMan();
+		var gameRunner = new GameRunner(game);
 		gameRunner.run();
 	}
 
--- 1. Basic-Spring-Framework/learning-spring-framework/src/main/java/com/spring/learning_spring_framework/game/SuperContraGame.java
+++ 1. Basic-Spring-Framework/learning-spring-framework/src/main/java/com/spring/learning_spring_framework/game/SuperContraGame.java
@@ -1,6 +1,6 @@
 package com.spring.learning_spring_framework.game;
 
-public class SuperContraGame {
+public class SuperContraGame implements GamingConsole {
     public void up() {
 		System.out.println("Up");
 	}
