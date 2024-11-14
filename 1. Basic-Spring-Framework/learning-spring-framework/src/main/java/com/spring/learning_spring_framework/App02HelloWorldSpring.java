619217a9d789e57cea268733de93675eb3a0ddbf
Borshon Saha <I-588@wgbd.com>
2024-11-14 17:58:00 +0600

    more spring bean added

--- 1. Basic-Spring-Framework/learning-spring-framework/src/main/java/com/spring/learning_spring_framework/App02HelloWorldSpring.java
+++ 1. Basic-Spring-Framework/learning-spring-framework/src/main/java/com/spring/learning_spring_framework/App02HelloWorldSpring.java
@@ -14,6 +14,9 @@ public class App02HelloWorldSpring {
 		
 //		3: Retrieving beans managed by beans
 		System.out.println(context.getBean("name"));
+		System.out.println(context.getBean("age"));
+		System.out.println(context.getBean("person"));
+		System.out.println(context.getBean("address"));
 	}
 
 }
--- 1. Basic-Spring-Framework/learning-spring-framework/src/main/java/com/spring/learning_spring_framework/HelloWorldConfiguration.java
+++ 1. Basic-Spring-Framework/learning-spring-framework/src/main/java/com/spring/learning_spring_framework/HelloWorldConfiguration.java
@@ -3,6 +3,11 @@ package com.spring.learning_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age) {};
+
+// Address - firstLine and city
+record Address(String firstLine, String city) {};
+
 @Configuration
 public class HelloWorldConfiguration {
 	
@@ -10,4 +15,22 @@ public class HelloWorldConfiguration {
 	public String name() {
 		return "Borshon";
 	}
+	
+	@Bean
+	public int age() {
+		return 25;
+	}
+	
+	@Bean
+	public Person person() {
+		var person = new Person("David", 20);
+		return person;
+	}
+	
+	@Bean
+	public Address address() {
+		var address = new Address("Bashundhara", "Dhaka");
+		return address;
+	}
+	
 }
